8de726b824fdbce6f9fe26820cb4e5f3fa154965
Alex Dyakonov <shram.monolit@mail.ru>
2025-07-08 12:13:32 +0300

    more fixes

--- client-new/src/components/ui/button.tsx
+++ client-new/src/components/ui/button.tsx
@@ -12,16 +12,33 @@ export const Button = ({
   children,
   disabled = false,
 }: ButtonProps) => {
+  const handleClick = (e: React.MouseEvent | React.TouchEvent) => {
+    e.preventDefault();
+    e.stopPropagation();
+    if (!disabled && onClick) {
+      onClick();
+    }
+  };
+
   return (
-    <div
+    <button
+      type="button"
       className={twMerge(
-        "text-[17px] w-fit bg-[#AFFF3F] text-black py-[18px] px-[30px] rounded-[30px] transition-colors",
+        "text-[17px] w-fit bg-[#AFFF3F] text-black py-[18px] px-[30px] rounded-[30px] transition-colors border-0 outline-none",
+        "touch-manipulation select-none", // iOS-specific optimizations
         disabled && "bg-gray-400 cursor-not-allowed",
         className
       )}
-      onClick={disabled ? undefined : onClick}
+      onClick={handleClick}
+      onTouchStart={() => {}} // iOS touch fix
+      disabled={disabled}
+      style={{ 
+        WebkitTapHighlightColor: 'transparent', // Remove iOS highlight
+        WebkitUserSelect: 'none',
+        userSelect: 'none'
+      }}
     >
       {children}
-    </div>
+    </button>
   );
 };
