efb4ff25d84578577a26903021e8936f976a5c66
PriestFaria <timurchikus@gmail.com>
2025-07-09 21:11:26 +0300

    padding bottom from nav

--- client-new/src/pages/main/layout.tsx
+++ client-new/src/pages/main/layout.tsx
@@ -1,20 +1,9 @@
-import { useEffect } from "react";
-import { viewport } from "@telegram-apps/sdk-react";
 import { AnimatedOutlet } from "../../components/helpers/animated-outlet";
 import BottomNavbar from "../../components/widgets/bottom-navbar";
 
 export const MainLayout = () => {
-  useEffect(() => {
-    // Расширяем viewport на весь экран
-    try {
-      viewport.expand();
-    } catch (error) {
-      console.warn("Viewport expansion not supported:", error);
-    }
-  }, []);
-
   return (
-    <div className="max-w-[90%] mx-auto">
+    <div className="max-w-[90%] mx-auto ">
       <AnimatedOutlet />
       <BottomNavbar />
     </div>
