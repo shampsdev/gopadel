96207c3e596a7fba99138d0e575224b63156d80e
Alex Dyakonov <shram.monolit@mail.ru>
2025-07-10 11:22:57 +0300

    removed py server

--- server/bot/init_bot.py
+++
@@ -1,8 +0,0 @@
-from aiogram import Bot
-from aiogram.client.default import DefaultBotProperties
-
-from config import settings
-
-bot = Bot(
-    token=settings.TG_API_TOKEN, default=DefaultBotProperties(parse_mode="Markdown")
-)
