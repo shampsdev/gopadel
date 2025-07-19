08ddc26f7610f484cd0309818f8178cb71212d47
PriestFaria <timurchikus@gmail.com>
2025-07-20 02:16:57 +0300

    added games home page

--- client-new/src/pages/home/home.tsx
+++ client-new/src/pages/home/home.tsx
@@ -1,11 +1,11 @@
-import { AnimatedOutlet } from "../../components/helpers/animated-outlet";
+import { Outlet } from "react-router";
 import { useTelegramBackButton } from "../../shared/hooks/useTelegramBackButton";
 
 export const Home = () => {
   useTelegramBackButton({ showOnMount: false, hideOnUnmount: false });
   return (
     <>
-      <AnimatedOutlet />
+      <Outlet />
     </>
   );
 };
